60861c8cc199fa6f9fdc184ef57d229cb1f7336b
Olga Myzova <89740104+Myzova@users.noreply.github.com>
2023-05-10 22:52:04 +0300

    Update Seminar5/example2.py
    Co-authored-by: Danil <49741194+samodurOFF@users.noreply.github.com>

--- Seminar5/example2.py
+++ Seminar5/example2.py
@@ -12,8 +12,7 @@ b = int(input('Введите второе целое неотрицательн
 def sum(a, b):
     if b == 0:
         return a
-    else:
-        return sum(a+1, b-1)
+    return sum(a + 1, b - 1)
 
 
 print(sum(a, b))
